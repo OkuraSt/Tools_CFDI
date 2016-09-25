6ccc4f664778b15b99cbb80865a8792e3661ada5
Vedha Ivan Barrientos Pantoja <vedha.ivan@gmail.com>
2016-09-25 00:58:14 -0500

    Archivo de Prueba del modulo main Prueba para la funcion generar_conceptos_cfdi

---
+++ test/test_main.py
@@ -0,0 +1,47 @@
+from os.path import join as join_path, dirname, abspath, isfile
+from main import generar_conceptos_cfdi
+from bs4 import BeautifulSoup
+
+# generar_conceptos_cfdi es una funcion que nos permite modificar
+# los conceptos de un cdfi desde una ruta dada, y generar nuevos a
+# partir de un arreglo de diccionarios que nos indiquen la informacion
+# requerida. La funcion regresa la ruta del nuevo archivo creado la cual
+# tambien se puede especificar en el parametro opcional file_to_create.
+def test_generar_conceptos_cfdi():
+    # Obtener la ruta del archivo
+    current_directory = dirname(abspath(__file__))
+    filename = join_path(current_directory, '..\\xmls\\2469.xml')
+
+    # Genera un diccionario con la informacion requerida
+    lista_conceptos = []
+    concepto_1 = {'cantidad': '3',
+                  'descripcion': 'Jabones',
+                  'importe': '64.2',
+                  'unidad': 'PZA',
+                  'valorUnitario': '21.4'}
+    concepto_2 = {'cantidad': '6',
+                  'descripcion': 'Shampoo',
+                  'importe': '313.8',
+                  'unidad': 'BOT',
+                  'valorUnitario': '52.3'}
+    lista_conceptos.append(concepto_1)
+    lista_conceptos.append(concepto_2)
+
+    ruta_archivo_nuevo = generar_conceptos_cfdi(filename, lista_conceptos)
+
+    # Verificar que el archivo existe
+    assert isfile(ruta_archivo_nuevo)
+    # Probar que el archivo generado tiene los conceptos enviados
+    with open(ruta_archivo_nuevo) as file:
+        xml = BeautifulSoup(file.read(), 'xml')
+        conceptos = xml.Comprobante.Conceptos.children
+        concepto_1_xml = conceptos.next()
+        assert concepto_1['cantidad'] == concepto_1_xml['cantidad']
+        assert concepto_1['descripcion'] == concepto_1_xml['descripcion']
+        assert concepto_1['importe'] == concepto_1_xml['importe']
+        assert concepto_1['valorUnitario'] == concepto_1_xml['valorUnitario']
+        concepto_2_xml = conceptos.next()
+        assert concepto_2['cantidad'] == concepto_2_xml['cantidad']
+        assert concepto_2['descripcion'] == concepto_2_xml['descripcion']
+        assert concepto_2['importe'] == concepto_2_xml['importe']
+        assert concepto_2['valorUnitario'] == concepto_2_xml['valorUnitario']
